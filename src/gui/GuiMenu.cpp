1e7644162b487802255528bd347e6d0b022f9e66
kolen <kolen>
2005-05-19 16:40:07 +0000

    Fixes. Added onMouseButton().

--- src/gui/GuiMenu.cpp
+++ src/gui/GuiMenu.cpp
@@ -1,12 +1,13 @@
-// $Id: GuiMenu.cpp,v 1.1 2005/05/19 15:00:50 kolen Exp $ 
+// $Id: GuiMenu.cpp,v 1.2 2005/05/19 16:40:07 kolen Exp $ 
 
 #include "GuiMenu.h"
 
-Sprite GuiMenu::gfx("menu.png");
+Sprite GuiMenu::gfx("menuitem.png");
 
 Rectangle GuiMenu::gfx_area_header (0, 0, MENUWIDTH, MENULINEHEIGHT);
 Rectangle GuiMenu::gfx_area_item (0, MENULINEHEIGHT, MENUWIDTH, MENULINEHEIGHT);
 Rectangle GuiMenu::gfx_area_footer (0, MENULINEHEIGHT*2, MENUWIDTH, MENULINEHEIGHT);
+Rectangle GuiMenu::gfx_area_item_sel (0, MENULINEHEIGHT*3, MENUWIDTH, MENULINEHEIGHT);
 
 GuiMenu::GuiMenu(int x, int y)
 {
@@ -14,29 +15,77 @@ GuiMenu::GuiMenu(int x, int y)
   rect.y = y;
   rect.w = MENUWIDTH;
   rect.h = MENULINEHEIGHT;
+  lastSelected = 0;
 }
 
 void
 GuiMenu::addItem(string text, string action)
 {
   MenuItem item = {text, action};
-  items.push_front(item);
+  items.push_back(item);
   rect.h += MENULINEHEIGHT;
 }
 
 void
 GuiMenu::onDraw()
 {
-  list<MenuItem>::iterator i;
-  int ii=0;
+  vector<MenuItem>::iterator i;
+  int ii=0, res;
   for (i=items.begin(); i!=items.end(); i++)
     {
       ii++;
       gfx.draw(rect.x, rect.y + ii*MENULINEHEIGHT,
-	       &gfx_area_item
+	       ((ii-1)==lastSelected) ? &gfx_area_item_sel : &gfx_area_item
 	       );
+      draw.text(i->text,
+                rect.x+MENUTEXTOFFSET_X ,
+                rect.y+MENUTEXTOFFSET_Y + ii*MENULINEHEIGHT 
+                );
     }
   gfx.draw(rect.x, rect.y + (ii+1)*MENULINEHEIGHT,
-	   &gfx_area_footer);
-  gfx.draw(rect.x, rect.y, &gfx_area_footer);
+	        &gfx_area_footer);
+  gfx.draw(rect.x, rect.y, &gfx_area_header);
+}
+
+int
+GuiMenu::getInFocus()
+{
+  int x, y, ys;
+  SDL_GetMouseState(&x, &y);
+  ys = (y-rect.y) / MENULINEHEIGHT;
+  ys--;
+  return ys;
+}
+
+void
+GuiMenu::onMouseFocus(char on)
+{
+  int thisSelected;
+  if (on) {
+    if (thisSelected = getInFocus() == lastSelected) {
+      lastSelected = thisSelected;
+      setDirty();
+    }
+  } else {
+    lastSelected = -1;
+    setDirty();
+  }
+}
+
+void
+GuiMenu::onMouseMotion(char button, int x, int y, int dx, int dy)
+{
+  int thisSelected = getInFocus();
+  if (thisSelected != lastSelected) {
+    lastSelected = thisSelected;
+    setDirty();
+  }
+}
+
+void
+GuiMenu::onMouseButton(char down, char button, int x, int y)
+{
+  int sel = getInFocus();
+  shell->execCode(items[sel].action);
+  shell->execCode("MenuRemove");
 }
--- src/gui/GuiMenu.h
+++ src/gui/GuiMenu.h
@@ -1,16 +1,21 @@
 #ifndef GUIMENU_H
 #define GUIMENU_H
 
-// $Id: GuiMenu.h,v 1.1 2005/05/19 15:00:50 kolen Exp $
+// $Id: GuiMenu.h,v 1.2 2005/05/19 16:40:07 kolen Exp $
 
-#include "../Sprite.h"
 #include "GuiWidget.h"
+#include "../Sprite.h"
+#include "../Shell.h"
+
 #include <string>
-#include <list>
+#include <vector>
+#include <iostream>
 using namespace std;
 
 #define MENUWIDTH 200
-#define MENULINEHEIGHT 32
+#define MENULINEHEIGHT 16
+#define MENUTEXTOFFSET_X 16
+#define MENUTEXTOFFSET_Y 2
 
 struct MenuItem
 {
@@ -24,13 +29,19 @@ class GuiMenu: public GuiWidget
   GuiMenu(int x, int y);
   void onDraw();
   void addItem(string text, string action);
+  void onMouseFocus(char on);
+  void onMouseMotion(char buttonstate, int x, int y, int dx, int dy);
+  void onMouseButton(char, char, int, int);
  private:
-  list<MenuItem> items;
-  
+  vector<MenuItem> items;
+  int lastSelected;
   static Sprite gfx;
   static Rectangle gfx_area_header;
   static Rectangle gfx_area_item;
   static Rectangle gfx_area_footer;
+  static Rectangle gfx_area_item_sel;
+ 
+  int getInFocus();
 
 };
 
